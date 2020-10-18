cc82fb3fb36c70f7fb6e4d7a8ec636bee37f3eff
luanbrito7 <lab7@cin.ufpe.br>
2020-10-18 12:57:23 -0300

    add form

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = values => console.log(values);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label for="age">Idade / Age</label><br/>
+            <input
+              name="age"
+              type="number"
+              ref={register({
+                required: "Esse campo é obrigatório",
+                validate: value => value > 0 && value < 200
+              })}
+            />
+            {errors.age?.type === "validate" &&
+              (
+                <div>
+                  <span className="error-message">É improvável que você tenha mais que duzentos ou menos que zero anos.</span>
+                </div>
+              )
+            }
+          </div>
+          <div>
+            <label for="death">Quantos anos estima viver?</label><br/>
+            <input
+              name="death"
+              type="number"
+              ref={register({
+                required: "Esse campo é obrigatório",
+                validate: value => value > 0 && value < 200
+              })}
+            />
+            {errors.death?.type === "validate" &&
+              (
+                <div>
+                  <span className="error-message">É improvável que você viva mais que duzentos ou menos que zero anos.</span>
+                </div>
+              )
+            }
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
